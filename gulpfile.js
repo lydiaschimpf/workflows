0d31f1fcce1e6287a136bbc2f104c99bdae9d545
booth11-mgr2 <booth11-mgr2@content.linkedin.biz>
2017-09-02 11:13:23 +0200

    Added gulp-coffee

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,10 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee');
 
-
-coffeeSources =['components/coffee/tagline.coffee'];
+coffeeScripts = ['components/coffee/tagline.coffee'];
 
 gulp.task('coffee', function(){
-  gulp.src(coffeeSources)
+  gulp.src(coffeeScripts)
 	.pipe(coffee({bare: true})
 	.on('error', gutil.log))
 	.pipe(gulp.dest('components/scripts'))
